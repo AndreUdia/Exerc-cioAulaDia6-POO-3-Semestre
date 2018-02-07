af7cbc8139a21608c3a361fe996b308bff2def5a
André <soutom@yahoo.com.br>
2018-02-06 22:16:55 -0200

    Add files via upload

---
+++ ExerciaDiaSeis4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExerciaDiaSeis4 {
+
+	public static void main(String[] args) {
+		//Escreva um programa Java para ler os valores de quatro números N1, N2,
+		//N3 e N4. Calcule e imprima o valor da média aritmética dos mesmos.
+		
+		Scanner in = new Scanner(System.in);
+		String um = in.nextLine();
+		String dois = in.nextLine();
+		String tres = in.nextLine();
+		String quatro = in.nextLine();
+		System.out.println(palavra);
+	}
+}
