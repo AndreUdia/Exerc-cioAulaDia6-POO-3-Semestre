6f77377b1007b918ad0136c9bcd00a0b49c6348d
André <soutom@yahoo.com.br>
2018-02-06 22:16:32 -0200

    Add files via upload

---
+++ ExerciaDiaSeis5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ExerciaDiaSeis5 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String palavra = in.nextLine();
+		System.out.println(palavra);
+	}
+}
