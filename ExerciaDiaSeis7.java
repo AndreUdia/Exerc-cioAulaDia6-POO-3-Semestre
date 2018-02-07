03bf0d7584e4fc156cc2e441aad46d3ecaa64e03
André <soutom@yahoo.com.br>
2018-02-06 22:33:49 -0200

    Add files via upload

---
+++ ExerciaDiaSeis7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ExerciaDiaSeis7 {
+
+	public static void main(String[] args) {
+		/*(for) Escreva um programa Java para imprimir natela todos os números
+ímpares de 299 a 101 em ordem decrescente*/
+		for(int i=299; i>100; i--) {
+			if(i%2 != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
