53309377db5aefee200774d2034c19891e26d09d
André <soutom@yahoo.com.br>
2018-02-06 22:29:18 -0200

    Add files via upload

---
+++ ExerciaDiaSeis6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ExerciaDiaSeis6 {
+
+	public static void main(String[] args) {
+		/*(while) Escreva um programa Java capaz de mostrar na tela do
+computador a seqüência de números de 1 a 100.*/
+		int x = 1;
+		while(x<=100) {
+			System.out.println(x);
+			x++;
+		}
+	}
+}
